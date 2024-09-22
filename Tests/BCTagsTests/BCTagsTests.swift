ced8d92c7cc53375cdf9806c59251fe0161f02ec
Wolf McNally <wolf@wolfmcnally.com>
2024-09-22 02:29:44 -0700

    Updated for latest Swift and Xcode.

--- Tests/BCTagsTests/BCTagsTests.swift
+++ Tests/BCTagsTests/BCTagsTests.swift
@@ -2,8 +2,13 @@ import Testing
 import BCTags
 
 struct BCTagsTests {
-    @Test @MainActor func example() throws {
+    @Test @MainActor func addTag() throws {
         addKnownTags()
+        let tag = Tag(12345, ["TestTag"])
+        globalTags.insert(tag)
+    }
+    
+    @Test func enumerateTags() {
         for tag in globalTags {
             print("\(String(describing: tag.name ?? "nil")) = \(tag.value)")
         }
